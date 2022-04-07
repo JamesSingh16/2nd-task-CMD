dd1ed4baa8fb8b089d09b31043944fef2a142d1e
JamesSingh16 <officialjames16@gmail.com>
2022-04-07 16:19:41 +0700

    2nd TaskCMD

---
+++ resources/views/index.blade.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Home | SmartSwap</title>
+  </head>
+  <body style="background-color:powderblue;">
+  <body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+    <a class="navbar-brand" href="{{('/index')}}">
+        <img src="s.png" width="30" height="30" class="d-inline-block align-top" name="SmartPay" alt="">
+        SmartPay 
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Trade
+                 </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Swap</a>
+                    <a class="dropdown-item" href="#">Exchange</a>
+                    <a class="dropdown-item" href="#">Liquidity</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Earn
+                 </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Farm</a>
+                    <a class="dropdown-item" href="#">Auto</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                NFT
+                 </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Collection</a>
+                    <a class="dropdown-item" href="#">Marketplace</a>
+                </div>
+            </li>
+        </ul>
+        
+            <img src="s.png" width="30" height="30" class="d-inline-block align-top" alt=""><b>$1</b>
+            <svg class="navbar-nav-svg" xmlns="#" viewBox="0 0 16 16" focusable="false" width="22 " height="20">
+                <path fill-rule="evenodd" d="M1.543 7.25h2.733c.144-2.074.866-3.756 1.58-4.948.12-.197.237-.381.353-.552a6.506 6.506 0 00-4.666 5.5zm2.733 1.5H1.543a6.506 6.506 0 004.666 5.5 11.13 11.13 0 01-.352-.552c-.715-1.192-1.437-2.874-1.581-4.948zm1.504 0h4.44a9.637 9.637 0 01-1.363 4.177c-.306.51-.612.919-.857 1.215a9.978 9.978 0 01-.857-1.215A9.637 9.637 0 015.78 8.75zm4.44-1.5H5.78a9.637 9.637 0 011.363-4.177c.306-.51.612-.919.857-1.215.245.296.55.705.857 1.215A9.638 9.638 0 0110.22 7.25zm1.504 1.5c-.144 2.074-.866 3.756-1.58 4.948-.12.197-.237.381-.353.552a6.506 6.506 0 004.666-5.5h-2.733zm2.733-1.5h-2.733c-.144-2.074-.866-3.756-1.58-4.948a11.738 11.738 0 00-.353-.552 6.506 6.506 0 014.666 5.5zM8 0a8 8 0 100 16A8 8 0 008 0z">
+                </path>
+            </svg>
+            <!-- <a href="#" class="badge badge-pill badge-info">Connect Wallet</a> -->
+            <a href="#" class="btn btn-outline-info mr-3">Connect Wallet</a>
+    </div>
+
+</nav>
+    </header>
+
+<style>
+    .card {
+    border: 5px;
+    border-radius: 30px;
+    /* position:absolute; */
+    left: 52%;
+    top: 25%;
+  
+    }
+
+</style>
+ 
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card mb-3">
+                <div class="card-body">
+                <p align="right"><img src="s.png" width="30" height="30" style="float:left;"><b>SPY-BUSD</p></b> 
+                <!-- <img src="s.png" width="30" height="30" class="" alt=""> 
+                <h5 class="card-title text-right" >SPY-BUSD</h5> -->
+                    <p class="card-text">APY: <b>1,001,3%</p></b>
+                    <p align="left" class="card-text">Earn: <b>SPAY-Fees</p></b>
+                    <p class="card-text">Harvest Lockup:   <b>12 Hour(s)</p></b>
+                    <p class="card-text"><small class="text-muted">SPY EARNED</small></p>
+                <div class="container text-center">
+                    <a href="#" class="btn btn-outline-info">Connect Wallet</a>
+                </div><br>
+    <ul class="list-group list-group-flush text-center">
+        <a href="#" li class="list-group-item">Details</li></a>
+    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-3">
+                <div class="card-body">
+                <p align="right"><img src="s.png" width="30" height="30" style="float:left;"><b>SPY-BNB</p></b> 
+                <!-- <img src="s.png" width="30" height="30" class="" alt=""> 
+                <h5 class="card-title text-right" >SPY-BUSD</h5> -->
+                    <p class="card-text">APY: <b>1,001,3%</p></b>
+                    <p align="left" class="card-text">Earn: <b>SPAY-Fees</p></b>
+                    <p class="card-text">Harvest Lockup:   <b>12 Hour(s)</p></b>
+                    <p class="card-text"><small class="text-muted">SPY EARNED</small></p>
+                <div class="container text-center">
+                    <a href="#" class="btn btn-outline-info">Connect Wallet</a>
+                </div><br>
+    <ul class="list-group list-group-flush text-center">
+        <a href="#" li class="list-group-item">Details</li></a>
+    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card border-dark mb-3">
+                <div class="card-body">
+                <p align="right"><img src="s.png" width="30" height="30" style="float:left;"><b>SPY-USDT</p></b> 
+                <!-- <img src="s.png" width="30" height="30" class="" alt=""> 
+                <h5 class="card-title text-right" >SPY-BUSD</h5> -->
+                    <p class="card-text">APY: <b>1,001,3%</p></b>
+                    <p align="left" class="card-text">Earn: <b>SPAY-Fees</p></b>
+                    <p class="card-text">Harvest Lockup:   <b>12 Hour(s)</p></b>
+                    <p class="card-text"><small class="text-muted">SPY EARNED</small></p>
+                <div class="container text-center">
+                    <a href="#" class="btn btn-outline-info">Connect Wallet</a>
+                </div><br>
+    <ul class="list-group list-group-flush text-center">
+        <a href="#" li class="list-group-item">Details</li></a>
+    </ul>
+                </div>
+            </div>
+        </div>  
+
+        <div class="col-sm-6">
+            <div class="card border-dark mb-3 w-50" style="margin-left:159px;">
+                <div class="card-body">
+                <p align="right"><img src="s.png" width="30" height="30" style="float:left;"><b>SPY-NFT</p></b> 
+                <!-- <img src="s.png" width="30" height="30" class="" alt=""> 
+                <h5 class="card-title text-right" >SPY-BUSD</h5> -->
+                    <p class="card-text">APY: <b>1,001,3%</p></b>
+                    <p align="left" class="card-text">Earn: <b>SPAY-Fees</p></b>
+                    <p class="card-text">Harvest Lockup:   <b>12 Hour(s)</p></b>
+                    <p class="card-text"><small class="text-muted">SPY EARNED</small></p>
+                <div class="container text-center">
+                    <a href="#" class="btn btn-outline-info">Connect Wallet</a>
+                </div><br>
+    <ul class="list-group list-group-flush text-center">
+        <a href="#" li class="list-group-item">Details</li></a>
+    </ul>
+                </div>
+            </div>
+        </div>  
+    </div>
+<!-- 
+    <footer class="footer-light bg-light">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    
+                    <p class="menu">
+                        <a href="#">Home</a>
+                        <a href="#">Agent</a>
+                        <a href="#">About</a>
+                        <a href="#">Listing</a>
+                        <a href="#">Blog</a>
+                        <a href="#">Contact</a>
+                    </p>
+                    <ul class="ftco-footer-social p-2">
+                        <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="ion-logo-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="ion-logo-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="ion-logo-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p class="copyright">
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                    </p>
+                </div>
+            </div>
+    </footer>    -->
+    <style>
+        .icon {
+            fill: blue;
+            width: 24px;
+            height: 24px;
+        }
+        .footer{
+            width: 100%;
+            height: 80px;
+            padding-left: 10px;
+            line-height: 20px;  
+            background: #eee;
+            color: #333;
+            position: absolute;
+            bottom: -325px;
+        }
+    </style>
+    <footer class="footer">
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <a href='#' class='tw'><svg class="icon" viewBox="0 0 24    24">
+                                <path d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path>
+                            </svg>
+                        <a href='#' class='telegram'><svg class="icon" viewBox="0 0 24    24">
+                            <path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z">
+                        </svg></a>
+                        
+                        <a href='#' class='telegram'><svg class="icon" viewBox="0 0 24    24">
+                            <path d="M11.344,5.71c0-0.73,0.074-1.122,1.199-1.122h1.502V1.871h-2.404c-2.886,0-3.903,1.36-3.903,3.646v1.765h-1.8V10h1.8v8.128h3.601V10h2.403l0.32-2.718h-2.724L11.344,5.71z"></path>
+                        </svg></a>
+                        <div class="footer-copyright text-center">© 2022 Copyright
+                            <a href="/">SmartPay.com</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
